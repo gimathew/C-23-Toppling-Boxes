c70b333dab5a76c8202b44456578fc5b1ec98288
gimathew <72664437+gimathew@users.noreply.github.com>
2020-12-12 11:52:13 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class Box {
+
+    constructor(x,y,width,height){
+
+        var options = {
+
+            restitution:0.5
+        }
+this.body=Bodies.rectangle(x,y,width,height,options);
+World.add(world,this.body);
+    }
+
+display(){
+    var angle = this.body.angle
+    push()
+    translate(this.body.position.x,this.body.position.y)
+    rotate(angle)
+    rectMode(CENTER)
+    fill("yellow")
+    rect(0,0,this.body.width,this.body.height)
+    pop()
+}
+
+
+
+
+
+}
